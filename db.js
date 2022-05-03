09b011882d7fc2d4bbf535f210542f0a3a99480e
eroshapdl <58264020+eroshapdl@users.noreply.github.com>
2022-05-03 15:05:54 +0545

    Add files via upload

---
+++ db.js
@@ -0,0 +1,26 @@
+const users = [
+    {
+        email: "eroshapaudel@gmail.com",
+        password: "password"
+    }
+];
+
+const publicPosts = [
+    {
+        title: "web contents",
+        content: "content"
+    }
+]
+
+const privatePosts = [
+    {
+        title: "patreon content",
+        content: "patreon contents"
+    }
+]
+
+module.exports = {
+    users,
+    publicPosts,
+    privatePosts
+}
